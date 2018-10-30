a62760495caf9ad23d163ea0738eaa022628b3dd
Matt <matthewgstillman@gmail.com>
2018-10-30 13:31:12 -0700

    First Commit

---
+++ homework_1.py
@@ -0,0 +1,85 @@
+name_location = "What is your name? Where are you from?"
+print(name_location)
+name_location_answer = input()
+print(str(name_location) + "\n " + str(name_location_answer))
+
+
+major_undecided = "What is your declared major? If undecided, let us know what are you interested in?"
+print(major_undecided)
+major_undecided_answer = input()
+print(str(major_undecided) + "\n " + str(major_undecided_answer))
+
+graduate_transfer = "When are you planning on graduating? Are you planning on transferring to a 4-year school?"
+print(graduate_transfer)
+graduate_transfer_answer = input()
+print(str(graduate_transfer) + "\n " + str(graduate_transfer_answer))
+
+company_goals = "What company would you love to work for and what would you love to do there?"
+print(company_goals)
+company_goals_answer = input()
+print(str(company_goals) + "\n " + str(company_goals_answer))
+
+
+coding_experience = "Have you coded before? If so, what languages?" 
+print(coding_experience)
+coding_experience_answer = input()
+print(str(coding_experience) + "\n " + str(coding_experience_answer))
+
+coding_challenges = "How was your experience about coding? What challenges did you face?"
+print(coding_challenges)
+coding_challenges_answer = input()
+print(str(coding_challenges) + "\n " + str(coding_challenges_answer))
+
+coding_anxiety = "Are you anxious or excited about coding?"
+print(coding_anxiety)
+coding_anxiety_answer = input()
+print(str(coding_anxiety) + "\n " + str(coding_anxiety_answer))
+
+python_topics = "What topics in Python are you looking to learn?"
+print(python_topics)
+python_topics_answer = input()
+print(str(python_topics) + "\n " + str(coding_anxiety_answer))
+
+other_info = "Anything else you want to share? Pets? Travel? Favorite restaurant, music, games?"
+print(other_info)
+other_info_answer = input()
+print(str(other_info) + "\n " + str(other_info_answer))
+
+
+#Output from Above Program
+# What is your name? Where are you from?
+#  My name is Matt Stillman, and I am originally from Salt Lake City, Utah. I currently live in San Jose
+# What is your name? Where are you from?
+#  My name is Matt Stillman, and I am originally from Salt Lake City, Utah. I currently live in San Jose
+# What is your declared major? If undecided, let us know what are you interested in?
+#  I don't have a declared major, but if I did, it would be CIS or Python
+# What is your declared major? If undecided, let us know what are you interested in?
+#  I don't have a declared major, but if I did, it would be CIS or Python
+# When are you planning on graduating? Are you planning on transferring to a 4-year school?
+#  I plan on looking for a job as a Python Developer
+# When are you planning on graduating? Are you planning on transferring to a 4-year school?
+#  I plan on looking for a job as a Python Developer
+# What company would you love to work for and what would you love to do there?
+#  I would love to work for one of the FAANG companies (Facebook, Apple, Amazon, Netflix, Google), but I would also be okay working at a startup as well
+# What company would you love to work for and what would you love to do there?
+#  I would love to work for one of the FAANG companies (Facebook, Apple, Amazon, Netflix, Google), but I would also be okay working at a startup as well
+# Have you coded before? If so, what languages?
+#  I have coded before - I have experience in HTML, CSS, JavaScript, Python, and I have a little experience in Java.
+# Have you coded before? If so, what languages?
+#  I have coded before - I have experience in HTML, CSS, JavaScript, Python, and I have a little experience in Java.
+# How was your experience about coding? What challenges did you face?
+#  My experience in coding was interesting, because I started out at an intensive coding bootcamp that was quite challenging, even for CS grads in attendance. My biggest challenge so far has been navigating the ups and downs of learning how to code
+# How was your experience about coding? What challenges did you face?
+#  My experience in coding was interesting, because I started out at an intensive coding bootcamp that was quite challenging, even for CS grads in attendance. My biggest challenge so far has been navigating the ups and downs of learning how to code
+# Are you anxious or excited about coding?
+#  Both - I'm always excited to learn about coding, although I sometimes get anxious when I get stuck on a problem. As I said, it's all part of the ups and downs of learning how to code
+# Are you anxious or excited about coding?
+#  Both - I'm always excited to learn about coding, although I sometimes get anxious when I get stuck on a problem. As I said, it's all part of the ups and downs of learning how to code
+# What topics in Python are you looking to learn?
+#  Classes, OOP, Syntax as well as the differences between Python 2 and Python 3, as I am making the transition between the two.
+# What topics in Python are you looking to learn?
+#  Both - I'm always excited to learn about coding, although I sometimes get anxious when I get stuck on a problem. As I said, it's all part of the ups and downs of learning how to code
+# Anything else you want to share? Pets? Travel? Favorite restaurant, music, games?
+#  I like to eat every day, travel when I can. My favorite type of food is Mexican Food. My pet cat's name is Grizzelda
+# Anything else you want to share? Pets? Travel? Favorite restaurant, music, games?
+#  I like to eat every day, travel when I can. My favorite type of food is Mexican Food. My pet cat's name is Grizzelda
